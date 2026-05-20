4116f7af489b0f1ddde9cf51f17213eaaa8086bb
maike william <maikewilliam.dev@gmail.com>
2026-05-20 20:17:12 -0300

    FEAT Logic was created for the menu text, in addition to functionalities DISING for responsive desktops.

--- src/components/sideBar/SideBarItem.jsx
+++ src/components/sideBar/SideBarItem.jsx
@@ -13,6 +13,11 @@ const SideBarItem = ({
 }) => {
 
     const {openTab} = useContext(WorkspaceContext)
+    const splitText = text ? text.split(' ') : "";
+
+    const firstText =  splitText.length > 0 ? splitText[0] + " " : "";
+    const secoundText = splitText.length >= 1 ? splitText[1] + " " : "";
+    console.log(secoundText);
 
     function handleClick(){
         if(item){
@@ -38,7 +43,8 @@ const SideBarItem = ({
                 )
             }
 
-            {text}
+            <span className="first">{firstText ? firstText : ""}</span>
+            <span className="secound">{secoundText ? secoundText : ""}</span>
 
         </li>
     );
