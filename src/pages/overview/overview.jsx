eb3fdc5bfdcc2307af5325cc1094dfe582edae89
maike william <maikewilliam.dev@gmail.com>
2026-08-23 17:03:38 -0300

    FEAT Add text describe for project

--- src/pages/overview/overview.jsx
+++ src/pages/overview/overview.jsx
@@ -1,3 +1,4 @@
+import { GrAlert } from "react-icons/gr";
 import "./styles.css";
 
 import {
@@ -25,6 +26,35 @@ const Overview = () => {
                     Express e MySQL.
                 </p>
             </header>
+
+            <div className="card-overview">
+                <article className="overview-card warning-att">
+
+                    <div className="overview-card-header">
+                        <GrAlert  />
+                        <strong>Ambiente de demonstração</strong>
+
+                    </div>
+
+                    
+                    <p>Este projeto está hospedado utilizando planos gratuitos/Hobby para fins de demonstração.</p>
+                    <p>O Front-end está hospedado na Vercel, a API/Back-end no Render e o banco de dados no Supabase.</p>
+                    <p>
+                        Devido às limitações desses planos, o servidor pode entrar em modo de inatividade quando não está sendo utilizado. 
+                        Por isso, a primeira requisição pode levar alguns segundos enquanto o serviço é reativado.
+                    </p>
+                    <p>
+                        Além disso, o armazenamento de arquivos no servidor não é persistente neste ambiente. 
+                        Portanto, imagens enviadas para os produtos podem ser removidas após reinicializações ou períodos de inatividade.
+                    </p>
+
+                    <p>
+                        Essas limitações são exclusivas do ambiente de demonstração e não representam 
+                        o comportamento esperado em uma infraestrutura de produção.
+                    </p>
+
+                </article>
+            </div>
 
             <div className="overview-grid">
 
@@ -72,9 +102,9 @@ const Overview = () => {
                         Context API, Reducers e serviços.
                     </p>
                 </article>
-
+                
             </div>
-
+   
             <footer className="overview-footer">
 
                 <TbDeviceDesktopAnalytics />
